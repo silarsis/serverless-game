225935ca90c49d5a8670f45f4a0866866eaa02bc
Kevin Littlejohn <kevin@littlejohn.id.au>
2020-01-26 09:06:27 +0800

    refactored lightstep env variables

--- aspects/handler.py
+++ aspects/handler.py
@@ -3,9 +3,6 @@ import logging
 
 # # LightStep recommended tracing
 # TODO: Commented out until I sort out the compilation requirements
-# from os import environ
-# environ['DD_TRACE_AGENT_URL'] = 'https://ingest.lightstep.com:443'
-# environ['DD_TRACE_GLOBAL_TAGS'] = "lightstep.service_name:serverless-game,lightstep.access_token:{}".format(environ['LIGHTSTEP_ACCESS_TOKEN'])
 # from ddtrace import tracer
 # from ddtrace.propagation.b3 import B3HTTPPropagator
 # tracer.configure(http_propagator=B3HTTPPropagator)
