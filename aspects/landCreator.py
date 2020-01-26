607f5dc7cc85bf010cdd8abef42d4b653225e79f
Kevin Littlejohn <kevin@littlejohn.id.au>
2020-01-26 09:55:42 +0800

    make landcreator a proper location, and remove the default table

--- aspects/landCreator.py
+++ aspects/landCreator.py
@@ -1,31 +1,24 @@
-from thing import Thing
 from handler import lambdaHandler
 from location import Location
 import random
 
 
-class LandCreator(Thing):
+class LandCreator(Location):
     " This entity creates new exits and moves "
-    def create(self):
-        # Land creators are mobs, so they're location aware
-        self.createAspect('Location')
-        super().create()
-
     def tick(self):
         # Get a list of exits in the location I'm in
-        my = self.aspect('Location')
         directions = {
             'north': 'south',
             'south': 'north',
             'west': 'east',
             'east': 'west'
         }
-        for loc in my.locations:
+        for loc in self.locations:
             # Randomly pick a direction - n, s, e, w
             exit = random.choice(directions)
             # If that exit already exists, take it
             if exit in loc.exits:
-                my.move(loc.uuid, loc.exits[exit])
+                self.move(loc.uuid, loc.exits[exit])
             # Otherwise, create a new exit with no land
             else:
                 new_loc = Location()
