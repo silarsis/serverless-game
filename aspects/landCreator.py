e9af8deafde76445908a5299fc6dab361b182239
Kevin Littlejohn <kevin@littlejohn.id.au>
2020-01-31 21:31:15 +0800

    rename to not use a builtin method

--- aspects/landCreator.py
+++ aspects/landCreator.py
@@ -24,16 +24,16 @@ class LandCreator(Location):
         }
         loc = Land(self.location, tid=self.tid)
         # Randomly pick a direction - n, s, e, w
-        exit = random.choice(list(directions.keys()))
+        chosen_exit = random.choice(list(directions.keys()))
         # If that exit already exists, take it
-        if exit in loc.exits:
-            self.move(loc.uuid, loc.exits[exit])
+        if chosen_exit in loc.exits:
+            self.move(loc.uuid, loc.exits[chosen_exit])
         # Otherwise, create a new exit with no land
         else:
-            new_loc = Land(uuid=loc.by_direction(exit))
-            new_loc.add_exit(directions[exit], loc.uuid)
-            loc.add_exit(exit, new_loc.uuid)
-            logging.info("I created a new piece of land, {} of here".format(exit))
+            new_loc = Land(uuid=loc.by_direction(chosen_exit))
+            new_loc.add_exit(directions[chosen_exit], loc.uuid)
+            loc.add_exit(chosen_exit, new_loc.uuid)
+            logging.info("I created a new piece of land, {} of here".format(chosen_exit))
         self.schedule_next_tick()
 
 
